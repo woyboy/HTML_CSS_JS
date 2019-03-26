69dc438771cdd1fc8b4857edcdac6744e1cc17dd
Matthew Woytovich <matt@Matthews-MacBook-Pro.local>
2019-03-26 06:18:26 -0400

    Adding solution to module 4

---
+++ module4-solution/script.js
@@ -0,0 +1,17 @@
+
+(function () {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (i = 0; i < names.length; i++) {
+
+    var firstLetter = names[i].charAt(0);
+    firstLetter = firstLetter.toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i])
+    }
+  }
+})();
